f1999e61b28c5884f228bcd944d29d0bc478609d
Lee Yanco <lee.yanco@gmail.com>
2017-06-30 12:30:08 -0400

    DRY up the parsers, convert state to expanded form

--- parse-address.rb
+++ parse-address.rb
@@ -1,10 +1,10 @@
 #$:.unshift File.expand_path("/Users/lyanco/workspace/fuigo/street-address", __FILE__)
-#require 'active_support/core_ext/string'
+require 'active_support/core_ext/string'
 require 'ruby_postal/parser'
 require 'csv'
 
 
-cols = {
+COLS = {
   display_name: 0,
   nickname: 1,
   email1: 2,
@@ -20,6 +20,67 @@ cols = {
   webpage: 12
 }
 
+STATE_ABBR_TO_NAME = {
+    'AL' => 'Alabama',
+    'AK' => 'Alaska',
+    'AS' => 'America Samoa',
+    'AZ' => 'Arizona',
+    'AR' => 'Arkansas',
+    'CA' => 'California',
+    'CO' => 'Colorado',
+    'CT' => 'Connecticut',
+    'DE' => 'Delaware',
+    'DC' => 'District of Columbia',
+    'FM' => 'Federated States Of Micronesia',
+    'FL' => 'Florida',
+    'GA' => 'Georgia',
+    'GU' => 'Guam',
+    'HI' => 'Hawaii',
+    'ID' => 'Idaho',
+    'IL' => 'Illinois',
+    'IN' => 'Indiana',
+    'IA' => 'Iowa',
+    'KS' => 'Kansas',
+    'KY' => 'Kentucky',
+    'LA' => 'Louisiana',
+    'ME' => 'Maine',
+    'MH' => 'Marshall Islands',
+    'MD' => 'Maryland',
+    'MA' => 'Massachusetts',
+    'MI' => 'Michigan',
+    'MN' => 'Minnesota',
+    'MS' => 'Mississippi',
+    'MO' => 'Missouri',
+    'MT' => 'Montana',
+    'NE' => 'Nebraska',
+    'NV' => 'Nevada',
+    'NH' => 'New Hampshire',
+    'NJ' => 'New Jersey',
+    'NM' => 'New Mexico',
+    'NY' => 'New York',
+    'NC' => 'North Carolina',
+    'ND' => 'North Dakota',
+    'OH' => 'Ohio',
+    'OK' => 'Oklahoma',
+    'OR' => 'Oregon',
+    'PW' => 'Palau',
+    'PA' => 'Pennsylvania',
+    'PR' => 'Puerto Rico',
+    'RI' => 'Rhode Island',
+    'SC' => 'South Carolina',
+    'SD' => 'South Dakota',
+    'TN' => 'Tennessee',
+    'TX' => 'Texas',
+    'UT' => 'Utah',
+    'VT' => 'Vermont',
+    'VI' => 'Virgin Island',
+    'VA' => 'Virginia',
+    'WA' => 'Washington',
+    'WV' => 'West Virginia',
+    'WI' => 'Wisconsin',
+    'WY' => 'Wyoming'
+  }
+
 def make_address(address, part)
   unless part.nil? || part.strip.empty?
     address += " "
@@ -32,61 +93,47 @@ def remove_commas(str)
   return str.gsub(/,/, "")
 end
 
-
-
-def get_line1(address)
+def combine_fields(address, matched_labels)
   output = ""
   address.each do |part|
-    if [:number, :house, :house_number, :road].include? part[:label]
+    if matched_labels.include? part[:label]
       output += remove_commas(part[:value])
       output += " "
     end
   end
   output.strip!
-  output += ","
+  return output
+end
+
+
+def get_line1(address)
+  output = combine_fields(address, [:number, :house, :house_number, :road])
   output = output.split(" ").map(&:capitalize).join(' ')
+  output += ","
   return output
 end
 
 def get_line2(address)
-  output = ""
-  address.each do |part|
-    if [:unit, :level, :staircase, :entrance, :po_box].include? part[:label]
-      output += remove_commas(part[:value])
-      output += " "
-    end
-  end
-  output.strip!
-  output += ","
+  output = combine_fields(address, [:unit, :level, :staircase, :entrance, :po_box])
   output = output.split(" ").map(&:capitalize).join(' ')
+  output += ","
   return output
 end
 
 def get_city(address)
-  output = ""
-  address.each do |part|
-    if [:city, :suburb, :city_district].include? part[:label]
-      output += remove_commas(part[:value])
-      output += " "
-    end
-  end
-  output.strip!
-  output += ","
+  output = combine_fields(address, [:city, :suburb, :city_district])
   output = output.split(" ").map(&:capitalize).join(' ')
+  output += ","
   return output
 end
 
 def get_state(address)
-  output = ""
-  address.each do |part|
-    if [:state, :state_district].include? part[:label]
-      output += remove_commas(part[:value])
-      output += " "
-    end
-  end
-  output.strip!
-  output += ","
+  output = combine_fields(address, [:state, :state_district])
   output = output.upcase
+  unless STATE_ABBR_TO_NAME[output].nil?
+    output = STATE_ABBR_TO_NAME[output]
+  end
+  output += ","
   return output
 end
 
@@ -99,8 +146,8 @@ def get_postcode(address)
     end
   end
   output.strip!
-  output += ","
   output = output.upcase
+  output += ","
   return output
 end
 
@@ -115,16 +162,9 @@ def make_US_postcode_5_digits(postcode)
 end
 
 def get_country(address)
-  output = ""
-  address.each do |part|
-    if [:country, :country_region].include? part[:label]
-      output += remove_commas(part[:value])
-      output += " "
-    end
-  end
-  output.strip!
-  output += ","
+  output = combine_fields(address, [:country, :country_region])
   output = output.upcase
+  output += ","
   return output
 end
 
@@ -137,13 +177,12 @@ def parse_address(row, cols)
   address = make_address(address, row[cols[:zip]].to_s)
   address = make_address(address, row[cols[:country]].to_s)
 
+  address.strip!
+  address.gsub!(/,/, "")
+  address.squish!
 
-  if !address.nil? && !address.empty?
-    address.strip!
-    address.gsub!(/,/, "")
-    address.gsub!(/\s+/," ")
-  else
-    address = nil
+  if address.empty? || address.blank?
+    address = nil #libpostal processes nil as [] but throws an exception for "" and " "
   end
 
   address = Postal::Parser.parse_address(address)
@@ -163,14 +202,14 @@ CSV_FILE_PATH = File.join("./input.csv")
 arr = CSV.read("./input.csv", :encoding => 'windows-1251:utf-8')
 #puts arr
 arr.each do |row|
-  unless row[cols[:display_name]].to_s.strip.empty?
+  unless row[COLS[:display_name]].to_s.strip.empty?
     output_row = ""
-    output_row += remove_commas(row[cols[:display_name]].to_s) + ","
-    output_row += remove_commas(row[cols[:email1]].to_s) + ","
-    output_row += remove_commas(row[cols[:phone1]].to_s) + ","
-    output_row += remove_commas(row[cols[:phone2]].to_s) + ","
-    output_row += parse_address(row, cols)
-    output_row += remove_commas(row[cols[:notes]].to_s) + " " + remove_commas(row[cols[:webpage]].to_s)
+    output_row += remove_commas(row[COLS[:display_name]].to_s) + ","
+    output_row += remove_commas(row[COLS[:email1]].to_s) + ","
+    output_row += remove_commas(row[COLS[:phone1]].to_s) + ","
+    output_row += remove_commas(row[COLS[:phone2]].to_s) + ","
+    output_row += parse_address(row, COLS)
+    output_row += remove_commas(row[COLS[:notes]].to_s) + " " + remove_commas(row[COLS[:webpage]].to_s)
     puts output_row
   end
 end
